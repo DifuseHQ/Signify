9371dda384de404bc6f82e9f96901457bfb5fbab
ajasma6570 <ajasmaprince@gmail.com>
2025-12-11 23:16:10 +0530

    difuse logo add in corporate clean template

--- src/lib/generator.ts
+++ src/lib/generator.ts
@@ -3,7 +3,7 @@ import { type Card, type SelectedColors } from './types';
 export function getDefaultCard(selectedColors: SelectedColors): Card {
     const hostname = window.location.hostname;
     const card: Card = {
-        template: 'modern-compact',
+        template: 'elegant-minimal',
         name: 'Hayzam Sherif',
         title: 'VP - Development',
         company: 'Iridia Solutions Private Limited',
--- src/lib/templates/elegant-minimal.svelte
+++ src/lib/templates/elegant-minimal.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import DIfuseLogo from '$lib/DIfuseLogo.svelte';
 	import { getIcons } from '$lib/icon';
 	import type { Card } from '$lib/types';
 	import { fade } from 'svelte/transition';
@@ -22,43 +23,40 @@
 		<table
 			id="email-signature"
 			width="100%"
-			cellspacing=""
+			cellspacing="0"
 			cellpadding="0"
 			border="0"
-			style="background-color: {card.colours
-				.background};  width: 100%; max-width: 580px; font-family: Arial, sans-serif; line-height: 1.5; margin: 0; border-radius: 8px;"
+			style="background-color:{card.colours
+				.background};max-width:580px;font-family:Arial, sans-serif;line-height:1.5;margin:0;border-radius:8px;"
 		>
 			<tbody>
 				<tr>
-					<td width="80" style="padding: 5px;">
+					<td width="80" style="padding:5px; vertical-align:middle;">
 						{#if card.photos.profile}
 							<img
 								src={card.photos.profile}
 								alt={card.name}
-								style="width: 80px; height: 70px;  border-radius: 50% / 40%; display: block;"
+								style="width:80px; height:80px; border-radius:50%; object-fit:cover; display:block;"
 							/>
 						{/if}
 					</td>
 
 					<td colspan="3">
-						<table
-							width="100%"
-							cellspacing="0"
-							cellpadding="0"
-							border="0"
-							style="margin-top: 25px;"
-						>
+						<table width="100%" cellspacing="0" cellpadding="0" border="0" style="margin-top:25px;">
 							<tbody>
 								<tr>
 									<td
-										style="font-size: 16px; font-weight: bold; color: {card.colours
-											.primary};padding-left:10px;"
+										style="font-size:16px;font-weight:bold;color:{card.colours
+											.primary};padding-left:10px;white-space:nowrap;"
 									>
 										{card.name}
 									</td>
 								</tr>
 								<tr>
-									<td style="font-size: 14px; color: {card.colours.text};padding-left:10px;">
+									<td
+										style="font-size:14px;color:{card.colours
+											.text};padding-left:10px;white-space:nowrap;"
+									>
 										{card.title} - {card.company}
 									</td>
 								</tr>
@@ -68,12 +66,12 @@
 				</tr>
 
 				<tr>
-					<td width="80" style="padding: 5px;">
+					<td width="80" style="padding:5px; vertical-align:middle;">
 						{#if card.photos.company}
 							<img
 								src={card.photos.company}
 								alt="Company Logo"
-								style="width: 80px; height: 70px; border-radius: 8px; display: block;"
+								style="width:80px; height:70px; border-radius:8px; display:block;"
 							/>
 						{/if}
 					</td>
@@ -84,61 +82,54 @@
 							cellspacing="0"
 							cellpadding="0"
 							border="0"
-							style="margin-bottom: 40px;"
+							style="margin-bottom:40px;"
 						>
 							<tbody>
 								<tr>
 									<td style="padding-left:10px;">
 										<a
-											target=" _blank"
-											href="mailto:{card.email}"
-											style="color: {card.colours
-												.text}; text-decoration: none;display: inline-block; text-align: right;margin-right:20px;white-space: nowrap;"
+											target="_blank"
+											href={'mailto:' + card.email}
+											style="color:{card.colours.text};text-decoration:none;white-space:nowrap;"
 										>
-											<span
-												style="display: flex; justify-content: flex-start; align-items: center;"
-											>
+											<span style="display:inline-flex; align-items:center;">
 												<img
 													src={icons['lucide:mail']}
 													alt="Email"
-													style="width: 16px; height: 16px; margin-right: 4px;"
+													style="width:16px; height:16px; margin-right:4px;"
 												/>
 												{card.email}
 											</span>
 										</a>
 									</td>
-									<td style="">
+
+									<td>
 										<a
-											href="tel:{card.phone}"
-											style="color: {card.colours
-												.text}; text-decoration: none; display: inline-block; text-align: right;margin-right:20px;white-space: nowrap;"
+											href={'tel:' + card.phone}
+											style="color:{card.colours.text};text-decoration:none;white-space:nowrap;"
 										>
-											<span
-												style="display: flex; justify-content: flex-start; align-items: center;"
-											>
+											<span style="display:inline-flex; align-items:center;">
 												<img
 													src={icons['mdi:phone']}
 													alt="Phone"
-													style="width: 16px; height: 16px; margin-right: 4px;"
+													style="width:16px; height:16px; margin-right:4px;"
 												/>
 												{card.phone}
 											</span>
 										</a>
 									</td>
-									<td style="">
+
+									<td>
 										<a
 											target="_blank"
 											href={card.websiteLink}
-											style="color: {card.colours
-												.text}; text-decoration: none; display: inline-block; text-align: right;white-space: nowrap;"
+											style="color:{card.colours.text};text-decoration:none;white-space:nowrap;"
 										>
-											<span
-												style="display: flex; justify-content: flex-start; align-items: center;"
-											>
+											<span style="display:inline-flex; align-items:center;">
 												<img
 													src={icons['mdi:web']}
 													alt="Website"
-													style="width: 16px; height: 16px; margin-right: 4px;"
+													style="width:16px; height:16px; margin-right:4px;"
 												/>
 												{card.website}
 											</span>
@@ -146,23 +137,36 @@
 									</td>
 								</tr>
 								<tr>
-									<td colspan="3" style="padding-left:9px;">
+									<td style="padding-top: 10px;padding-left:10px;text-align:left">
 										<a
 											href={card.location}
 											target="_blank"
-											style="color: {card.colours.text}; text-decoration: none;display:flex;"
+											style="color:{card.colours.text};text-decoration:none;white-space:nowrap;"
 										>
-											<span
-												style="display: flex; justify-content: flex-start; align-items: center;white-space: nowrap;"
-											>
+											<span style="display:inline-flex; align-items:center;">
 												<img
 													src={icons['mdi:map-marker']}
 													alt="Location"
 													width="16"
 													height="16"
-													style="vertical-align: middle;margin-right: 4px;"
+													style="margin-right:4px;"
 												/>
 												<span>{card.location}</span>
+											</span>
+										</a>
+									</td>
+
+									<td
+										style="padding-top: 10px;padding-left:10px;text-align:right;padding-right: 10px;"
+									>
+										<a
+											href="https://signify.difuse.io/"
+											target="_blank"
+											style="color:{card.colours.text};text-decoration:none;white-space:nowrap;"
+										>
+											<span style="display:inline-flex; align-items:center;gap: 4px;">
+												<DIfuseLogo />
+												<span>Create your own signature</span>
 											</span>
 										</a>
 									</td>
