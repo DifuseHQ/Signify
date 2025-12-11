62983820f1305d05512b47c6d452a091f71cf811
ajasma6570 <ajasmaprince@gmail.com>
2025-12-11 22:46:47 +0530

    difuse logo add in professional grid template

--- src/lib/generator.ts
+++ src/lib/generator.ts
@@ -1,49 +1,49 @@
 import { type Card, type SelectedColors } from './types';
 
 export function getDefaultCard(selectedColors: SelectedColors): Card {
-	const hostname = window.location.hostname;
-	const card: Card = {
-		template: 'corporate-highlight',
-		name: 'Hayzam Sherif',
-		title: 'VP - Development',
-		company: 'Iridia Solutions Private Limited',
-		email: 'hayzam@difuse.io',
-		phone: '+91-4973540633',
-		website: 'difuse.io',
-		websiteLink: 'https://difuse.io',
-		location: 'Chennai, India',
-		linkedIn: 'https://www.linkedin.com/company/iridiasolutions/',
-		twitter: 'https://twitter.com/DifuseHQ',
-		colours: selectedColors,
-		photos: {
-			profile:
-				'https://avatars.githubusercontent.com/u/3922884?s=400&u=441b2ae32a36f919fa3dc4bea6ce478f0f42e1fc&v=4',
-			company: 'https://downloads-bucket.difuse.io/98e83c79_logo-only.png'
-		},
-		custom: {
-			corporateHighlight: {
-				photos: {
-					company: 'https://adhoc-bucket.iptechnics.com/ipt-sig-assets/logo4.png'
-				},
-				tollFree: '800-LINUX',
-				mobile: '+971-50-1234567'
-			}
-		}
-	};
+    const hostname = window.location.hostname;
+    const card: Card = {
+        template: 'professional-grid',
+        name: 'Hayzam Sherif',
+        title: 'VP - Development',
+        company: 'Iridia Solutions Private Limited',
+        email: 'hayzam@difuse.io',
+        phone: '+91-4973540633',
+        website: 'difuse.io',
+        websiteLink: 'https://difuse.io',
+        location: 'Chennai, India',
+        linkedIn: 'https://www.linkedin.com/company/iridiasolutions/',
+        twitter: 'https://twitter.com/DifuseHQ',
+        colours: selectedColors,
+        photos: {
+            profile:
+                'https://avatars.githubusercontent.com/u/3922884?s=400&u=441b2ae32a36f919fa3dc4bea6ce478f0f42e1fc&v=4',
+            company: 'https://downloads-bucket.difuse.io/98e83c79_logo-only.png'
+        },
+        custom: {
+            corporateHighlight: {
+                photos: {
+                    company: 'https://adhoc-bucket.iptechnics.com/ipt-sig-assets/logo4.png'
+                },
+                tollFree: '800-LINUX',
+                mobile: '+971-50-1234567'
+            }
+        }
+    };
 
-	if (hostname === 'signify.iptechnics.com') {
-		card.template = 'professional-grid';
-		card.title = 'Open Source Evangelist';
-		card.company = 'IP Technics L.L.C';
-		card.email = 'hayzam@iptechnics.com';
-		card.phone = '+971-4-2142025';
-		card.website = 'www.iptechnics.com';
-		card.websiteLink = 'https://www.iptechnics.com';
-		card.location = 'Dubai, UAE';
-		card.linkedIn = 'https://www.linkedin.com/company/ip-technics';
-		card.twitter = '';
-		card.photos.company = 'https://downloads-bucket.difuse.io/9e3293c2_iptechnics-logo.png';
-	}
+    if (hostname === 'signify.iptechnics.com') {
+        card.template = 'professional-grid';
+        card.title = 'Open Source Evangelist';
+        card.company = 'IP Technics L.L.C';
+        card.email = 'hayzam@iptechnics.com';
+        card.phone = '+971-4-2142025';
+        card.website = 'www.iptechnics.com';
+        card.websiteLink = 'https://www.iptechnics.com';
+        card.location = 'Dubai, UAE';
+        card.linkedIn = 'https://www.linkedin.com/company/ip-technics';
+        card.twitter = '';
+        card.photos.company = 'https://downloads-bucket.difuse.io/9e3293c2_iptechnics-logo.png';
+    }
 
-	return card;
+    return card;
 }
--- src/lib/templates/professional-grid.svelte
+++ src/lib/templates/professional-grid.svelte
@@ -4,6 +4,7 @@
 	import { generateQR, generateVCard } from '$lib/utils';
 	import { fade } from 'svelte/transition';
 	import { extraInputs } from '$lib/extra-inputs.svelte';
+	import DIfuseLogo from '$lib/DIfuseLogo.svelte';
 
 	interface Props {
 		card: Card;
@@ -32,174 +33,166 @@
 			cellspacing="5"
 			cellpadding="0"
 			border="0"
-			style="background-color: {card.colours
-				.background};  width: 100%; max-width: 580px; font-family: Arial, sans-serif; line-height: 1.5; margin: 0; padding: 5px;  border-radius: 8px;"
+			style="background-color:{card.colours
+				.background};max-width:580px;font-family:Arial, sans-serif;line-height:1.5;margin:0;padding:5px;border-radius:8px;"
 		>
 			<tbody>
 				<tr>
-					<td width="80" style="vertical-align: middle; padding: 5px;">
+					<td width="80" style="vertical-align:middle; padding:5px;">
 						{#if card.photos.profile}
 							<img
 								src={card.photos.profile}
 								alt={card.name}
-								style="width: 80px; height: 80px; display: block;border-radius:8px"
+								style="width:80px; height:80px; display:block; border-radius:8px;"
 							/>
 						{/if}
 					</td>
-					<td colspan="2" style="padding-left: 10px;">
-						<strong style="font-size: 16px; color: {card.colours.primary};white-space: nowrap;"
-							>{card.name}</strong
-						><br />
-						<span style="font-size: 14px; color: {card.colours.text};white-space: nowrap;"
-							>{card.title}</span
-						><br />
-						<span style="font-size: 14px; color: {card.colours.text};white-space: nowrap;"
-							>{card.company}</span
-						>
+
+					<td colspan="2" style="padding-left:10px;">
+						<strong style="font-size:16px;color:{card.colours.primary};white-space:nowrap;">
+							{card.name}
+						</strong><br />
+						<span style="font-size:14px;color:{card.colours.text};white-space:nowrap;">
+							{card.title}
+						</span><br />
+						<span style="font-size:14px;color:{card.colours.text};white-space:nowrap;">
+							{card.company}
+						</span>
 					</td>
-					<td width="128" style="text-align: right; padding: 5px;">
+
+					<td width="128" style="text-align:right; padding:5px;">
 						{#if qrCode !== ''}
 							<img
 								src={qrCode}
 								alt="QR Code"
-								style="width: 128px; height: 128px; display: block; padding: 5px;"
+								style="width:128px; height:128px; display:block; padding:5px;"
 							/>
 						{/if}
 					</td>
 				</tr>
 				<tr>
-					<td rowspan="2" width="80" style="vertical-align: middle; padding: 5px;">
+					<td rowspan="2" width="80" style="vertical-align:middle; padding:5px;">
 						{#if card.photos.company}
 							<img
 								src={card.photos.company}
 								alt={card.company}
-								style="width: 80px; height: 80px; display: block;border-radius:8px"
+								style="width:80px; height:80px; display:block; border-radius:8px;"
 							/>
 						{/if}
 					</td>
-					<td colspan="3" style="padding-top: 10px; border-top: 1px solid {card.colours.primary};">
-						<table cellspacing="0" cellpadding="0" border="0" width="100%">
+
+					<td colspan="3" style="padding-top:10px; border-top:1px solid {card.colours.primary};">
+						<table width="100%" cellspacing="0" cellpadding="0" border="0">
 							<tbody>
 								<tr>
-									<td style="padding-bottom: 5px;padding-left:10px">
+									<td style="padding-bottom:5px; padding-left:10px; text-align:left;">
 										<a
-											target=" _blank"
-											href="mailto:{card.email}"
-											style="color: {card.colours
-												.text}; text-decoration: none;display: inline-block; text-align: right;margin-right:20px;white-space: nowrap;"
+											target="_blank"
+											href={'mailto:' + card.email}
+											style="color:{card.colours.text};text-decoration:none;white-space:nowrap;"
 										>
-											<span
-												style="display: flex; justify-content: flex-start; align-items: center;"
-											>
+											<span style="display:inline-flex; align-items:center;">
 												<img
 													src={icons['lucide:mail']}
 													alt="Email"
-													style="width: 16px; height: 16px; margin-right: 4px;"
+													style="width:16px; height:16px; margin-right:4px;"
 												/>
 												{card.email}
 											</span>
 										</a>
 									</td>
-									<td style="padding-bottom: 5px;padding-left:10px">
+
+									<td
+										style="padding-bottom:5px; padding-left:10px; text-align:right;padding-right: 10px;"
+									>
 										<a
-											href="tel:{card.phone}"
-											style="color: {card.colours
-												.text}; text-decoration: none; display: inline-block; text-align: right;margin-right:20px;white-space: nowrap;"
+											href={'tel:' + card.phone}
+											style="color:{card.colours.text};text-decoration:none;white-space:nowrap;"
 										>
-											<span
-												style="display: flex; justify-content: flex-start; align-items: center;"
-											>
+											<span style="display:inline-flex; align-items:center;">
 												<img
 													src={icons['mdi:phone']}
 													alt="Phone"
-													style="width: 16px; height: 16px; margin-right: 4px;"
+													style="width:16px; height:16px; margin-right:4px;"
 												/>
 												{card.phone}
 											</span>
 										</a>
 									</td>
 								</tr>
+
 								<tr>
-									<td style="padding-bottom: 5px;padding-left:10px">
+									<td style="padding-bottom:5px; padding-left:10px; text-align:left;">
 										<a
 											target="_blank"
 											href={card.websiteLink}
-											style="color: {card.colours
-												.text}; text-decoration: none; display: inline-block; text-align: right;white-space: nowrap;"
+											style="color:{card.colours.text};text-decoration:none;white-space:nowrap;"
 										>
-											<span
-												style="display: flex; justify-content: flex-start; align-items: center;"
-											>
+											<span style="display:inline-flex; align-items:center;">
 												<img
 													src={icons['mdi:web']}
 													alt="Website"
-													style="width: 16px; height: 16px; margin-right: 4px;"
+													style="width:16px; height:16px; margin-right:4px;"
 												/>
 												{card.website}
 											</span>
 										</a>
 									</td>
-									<td style="padding-bottom: 5px;padding-left:10px">
+
+									<td
+										style="padding-bottom:5px; padding-left:10px; text-align:right;padding-right: 10px;"
+									>
 										<a
 											href={card.location}
 											target="_blank"
-											style="color: {card.colours.text}; text-decoration: none;display:flex;"
+											style="color:{card.colours.text};text-decoration:none;white-space:nowrap;"
 										>
-											<span
-												style="display: flex; justify-content: flex-start; align-items: center;white-space: nowrap;"
-											>
+											<span style="display:inline-flex; align-items:center;">
 												<img
 													src={icons['mdi:map-marker']}
 													alt="Location"
 													width="16"
 													height="16"
-													style="vertical-align: middle;margin-right: 4px;"
+													style="margin-right:4px;"
 												/>
 												<span>{card.location}</span>
 											</span>
 										</a>
 									</td>
 								</tr>
+
 								<tr>
-									<td colspan="2" style="padding-bottom:5px;padding-left: 10px;display:flex;">
-										<!-- {#if card.linkedIn}
+									<td style="padding-bottom:5px; padding-left:10px; text-align:left;">
+										{#each extraInputs.socialInputs as social}
 											<a
-												href={card.linkedIn}
+												href={social.value}
 												target="_blank"
-												style="text-decoration: none; margin-right: 10px;"
+												style="text-decoration:none; margin-right:8px; display:inline-block;"
 											>
 												<img
-													src={icons['mdi:linkedin']}
-													alt="LinkedIn"
+													src={icons[social.icon]}
+													alt={social.id}
 													width="16"
 													height="16"
-													style="vertical-align: middle;"
-												/>
-											</a>
-										{/if}
-										{#if card.twitter}
-											<a href={card.twitter} target="_blank" style="text-decoration: none;">
-												<img
-													src={icons['mdi:twitter']}
-													alt="Twitter"
-													width="16"
-													height="16"
-													style="vertical-align: middle;"
-												/>
-											</a>
-										{/if} -->
-
-										{#each extraInputs.socialInputs as card}
-											<a href={card.value} target="_blank" style="text-decoration: none;">
-												<img
-													src={icons[card.icon]}
-													alt={card.id}
-													width="16"
-													height="16"
-													style="vertical-align: middle;"
+													style="vertical-align:middle;"
 												/>
 											</a>
 										{/each}
+									</td>
+
+									<td
+										style="padding-bottom:5px; padding-left:10px; text-align:right;padding-right: 10px;"
+									>
+										<a
+											href="https://signify.difuse.io/"
+											target="_blank"
+											style="color:{card.colours.text};text-decoration:none;white-space:nowrap;"
+										>
+											<span style="display:inline-flex; align-items:center;gap: 4px;">
+												<DIfuseLogo />
+												<span>Create your own signature</span>
+											</span>
+										</a>
 									</td>
 								</tr>
 							</tbody>
